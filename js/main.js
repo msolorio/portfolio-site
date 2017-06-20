3091c2ff78e07d1bf3831c8989b2c9fda24e1d1a
msolorio <msolorio789@gmail.com>
2017-06-20 11:43:35 -0700

    add fixed header functionality and styles

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function setYear() {
+  var currentYear = new Date().getFullYear();
+  $('.js-currentYear').html(currentYear);
+}
+
+// returns true if given element is within viewport or above viewport
+$.fn.hasScrolledUpTo = function() {
+  var win = $(window);
+  var viewport = {};
+
+  // scrollTop returns the scroll position from top
+  viewport.top = win.scrollTop();
+  viewport.bottom = viewport.top + win.height();
+  
+  // offset will return the current coordinates of the element
+  var bounds = this.offset();
+  bounds.bottom = bounds.top + this.outerHeight();
+
+  return (!(viewport.bottom < bounds.top));
+};
+
+function setFixedHeader() {
+  $(window).on('scroll', function() {
+    if ($('.workExamples').hasScrolledUpTo() && !($('.footer').hasScrolledUpTo())) {
+      $('.js-hero_header-fixed').fadeIn(2500);
+    } else {
+      $('.js-hero_header-fixed').fadeOut(400);
+    }
+  });
+}
+
+$(document).ready(function() {
+  setYear();
+  setFixedHeader();
+});
